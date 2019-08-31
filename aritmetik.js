af35af303efa1e798c2fc6ec442d039ef1071174
xgent1 <ihsan.ylc51@gmail.com>
2019-08-31 21:58:02 +0300

    js dersleri

---
+++ aritmetik.js
@@ -0,0 +1,12 @@
+/*Sayıyı 1 artırmak için "++" kullanılır "++" sola yazarsan değeri artırıp yazar. Sağa yazarsan yazdırıp değerini artırır.*/
+
+var sayi = 10;
+
+console.log(++sayi);
+
+
+/*2 değerle işlem yaparken böyle yapılabilir yazdırıldığında iki sayıyı çarparak yazdırır.*/
+var sayi = 5;
+var yeniSayi = (sayi *= 2);
+
+console.log(yeniSayi);
